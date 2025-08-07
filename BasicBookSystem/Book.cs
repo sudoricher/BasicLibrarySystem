49949a03017221c0afa24c050e1d394907ca9a7a
Bonginkosi Moatshe <mobonginkosi@gmail.com>
2025-08-07 19:21:46 +0200

    Add project files.

---
+++ BasicBookSystem/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BasicBookSystem
+{
+    public class Book
+    {
+        private string _title;
+        public string Title {
+            get { return _title; }
+            set
+            {
+                _title = value;
+            }
+        }
+        public string Author { get; set; }
+
+        private int _numberOfPages;
+        public int NumberOfPages
+        {
+            get { return _numberOfPages; }
+            set
+            {
+                if (value > 0) _numberOfPages = value;
+            }
+        }
+        public Book(string title, string author, int numberOfPages)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.NumberOfPages = numberOfPages;
+        }
+        public void DescribeBook()
+        {
+            string summary = $"Title: {Title} Author: {Author} Pages:{NumberOfPages}";
+            Console.WriteLine(summary);
+        }
+
+    }
+}
---
+++ BasicBookSystem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book theAlchemist = new Book("The Alchemist", "Paulo Coelho", 208);
+            theAlchemist.DescribeBook();
+            Book atomicHabits = new Book("Atomic Habits", "James Clear", 320);
+            atomicHabits.DescribeBook();
+        }
+    }
+}
